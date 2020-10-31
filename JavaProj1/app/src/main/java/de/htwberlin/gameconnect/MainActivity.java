e12a4b8410f0ab43777a33f8015f5a9329dd46be
s0567271 <adib.waluya@student.htw-berlin.de>
2020-10-31 20:23:08 +0100

    added tag for each of the chips

--- JavaProj1/app/src/main/java/de/htwberlin/gameconnect/MainActivity.java
+++ JavaProj1/app/src/main/java/de/htwberlin/gameconnect/MainActivity.java
@@ -8,11 +8,25 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 0: yellow; 1: red; 2: empty
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int activePlayer = 0;
+
     public void dropIn(View view) {
 
         ImageView counter = (ImageView)view;            // Access the variable from the argument
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedCounter] = activePlayer;
         counter.setTranslationY(-1500);                 // Start from out of the screen
-        counter.setImageResource(R.drawable.yellow);
+
+        if(activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);  // return the chip
     }
 
