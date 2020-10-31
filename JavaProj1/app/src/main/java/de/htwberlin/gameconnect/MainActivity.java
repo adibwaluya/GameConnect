9429c70f83b1adbeaaffb37f15313b8646dff918
s0567271 <adib.waluya@student.htw-berlin.de>
2020-10-31 20:09:52 +0100

    Reconfigured the size of layoutmargin and add dropIn function

--- JavaProj1/app/src/main/java/de/htwberlin/gameconnect/MainActivity.java
+++ JavaProj1/app/src/main/java/de/htwberlin/gameconnect/MainActivity.java
@@ -3,8 +3,18 @@ package de.htwberlin.gameconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView)view;            // Access the variable from the argument
+        counter.setTranslationY(-1500);                 // Start from out of the screen
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);  // return the chip
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
